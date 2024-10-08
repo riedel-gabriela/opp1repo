1dbf2c68da07b5fd3bf1096e696de064a428a9c8
gabriela <gabriela@email.com>
2024-10-08 20:19:33 -0300

    add: texto.txt and changed main

--- aula-05/app/src/main/java/org/example/App.java
+++ aula-05/app/src/main/java/org/example/App.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        scanner.close();
-        System.out.printf("Olá, %s!", nome);
+        while(scanner.hasNext()) {
+            String linha = scanner.nextLine();
+            System.out.println(linha.toUpperCase());
+        }
     }
 }
